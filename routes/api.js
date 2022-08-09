167af00549bae88862fe71ea204a276688520fb4
Ashwin202 <ashes192000@gmail.com>
2022-08-09 21:13:36 +0530

    fixed feedback uploading

--- routes/api.js
+++ routes/api.js
@@ -16,12 +16,12 @@ router.get("/login", (req, res) => {
   if (req.user) {
     if (sessionData.user.userType != '' && sessionData.user.userType == 'guest') {
       con.query(query.getAllDetailsGuest(req.user.empid), (error, result) => {
-        return res.status(200).render("../views/dashboard.ejs", { userData: result })
+        return res.status(200).render("../views/dashboard-mentor.ejs", {usertype: sessionData.user.userType, userData: result })
       })
     }
     else {
       con.query(query.getAllDetails(req.user.empid), (error, result) => {
-        return res.status(200).render("../views/dashboard-mentor.ejs", { userData: result })
+        return res.status(200).render("../views/dashboard-mentor.ejs", { usertype: sessionData.user.userType,userData: result })
       })
     }
   }
@@ -54,10 +54,11 @@ router.get('/altFailure', (req, res) => {
 
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
   con.query(query.getAllDetails(req.user.empid), (error, result) => {
+    console.log(sessionData.user.userType)
     return res.status(200).render("../views/dashboard-mentor.ejs", { usertype: sessionData.user.userType, userData: result })
   })
 })
-router.get("/feedback", ensureAuthenticated, (req, res) => {
+router.get("/feedback", ensureAuthenticated, (req, res) => {  
   return res.status(200).render("../views/feedback.ejs")
 })
 router.post("/feedback", (req, res) => {
@@ -65,6 +66,7 @@ router.post("/feedback", (req, res) => {
   let mentorid = req.body.mentorid
   let menteename = req.body.menteename
   let menteeid = req.body.menteeid
+  let domain = req.body.domain
   let punctuality = req.body.punctuality
   let dedication = req.body.dedication
   let behaviour = req.body.behaviour
@@ -73,8 +75,24 @@ router.post("/feedback", (req, res) => {
   let question = req.body.question
   let advice = req.body.advice
   let listen = req.body.listen
-
-  // con.query(query.)
+  let comments=req.body.comments
+console.log(req.body)
+  con.query(query.addFeedback(menteeid,menteename,mentorname,mentorid,domain,comments,punctuality,dedication,behaviour,work,study,question,advice,listen),(err,result)=>{
+    if(err){
+      console.log(err.message)
+    }
+    else{
+      console.log("Feedback added")   
+      res.status(200).json({
+        error: true,
+        message: 'Invalid Special Characters used in Username/Password',
+        data: {}
+    })
+      // response.status(400).json({ error: true, message: 'Agent Inactive' })   
+      // res.status(200).json({message:"SUCCESS"})
+      return
+      }
+  })
 })
 router.get("/logout", (req, res) => {
   req.logout(function (err) {
