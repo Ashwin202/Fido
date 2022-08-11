aa2f3c07b0a2f63ff4edc622368f7a962849b492
Ashwin202 <ashes192000@gmail.com>
2022-08-12 01:53:43 +0530

    Export option added-frontend

--- public/plugins/notification.js
+++ public/plugins/notification.js
@@ -12,7 +12,7 @@ demo = {
                 x: 30, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
                 y: 5 // vertical axis - can be a number or a string indicating unity. eg: '2em'
             },style:{
-                width:"18%"
+                // width:"18%"
             },
         }).showToast();
     }
